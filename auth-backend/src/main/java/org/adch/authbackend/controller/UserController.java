99785cb828ce204f30cbf01b4d403f33856a92d3
AChatatou <adil.chtatou@gmail.com>
2026-04-16 14:11:18 +0200

    Correcting user creation route

--- auth-backend/src/main/java/org/adch/authbackend/controller/UserController.java
+++ auth-backend/src/main/java/org/adch/authbackend/controller/UserController.java
@@ -8,11 +8,13 @@ import org.adch.authbackend.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @AllArgsConstructor
-@RestController("/auth")
+@RestController
+@RequestMapping("/auth")
 public class UserController {
 
     private UserService userService;
